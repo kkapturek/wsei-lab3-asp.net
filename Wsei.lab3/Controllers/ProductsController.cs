19476bb582207e1797ead0f69824c3988a87fccb
kkapturek <80354389+kkapturek@users.noreply.github.com>
2021-11-19 14:17:29 +0100

    add database support

--- Wsei.lab3/Controllers/ProductsController.cs
+++ Wsei.lab3/Controllers/ProductsController.cs
@@ -3,19 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Wsei.lab3.Database;
+using Wsei.lab3.Entities;
 using Wsei.lab3.Models;
 
 namespace Wsei.lab3.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly AppDbContext _dbContext;
+        public ProductsController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Add(ProductModel product)
+        public async Task <IActionResult> Add(ProductModel product)
         {
+            var entity = new ProductEntity 
+            {
+                Name = product.Name,
+                Description = product.Description,
+                IsVisible = product.IsVisible,
+            };
+
+            await _dbContext.Products.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+
             var viewModel = new ProductStatsViewModel
             {
                 NameLength = product.Name.Length,
---
+++ Wsei.lab3/Database/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wsei.lab3.Entities;
+
+namespace Wsei.lab3.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<ProductEntity> Products { get; set; }
+        public AppDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+    }
+}
